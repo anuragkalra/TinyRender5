fd1db7e75e6dd1812848c304e5aca09ee28bcdea
anuragkalra <anuragkalra@outlook.com>
2018-11-17 17:58:25 -0500

    initial setup

---
+++ cmake-build-debug/CMakeFiles/tinyrender.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/anuragkalra/workspaceComp598/a5_starter/a5/src/core/integrator.cpp" "/Users/anuragkalra/workspaceComp598/a5_starter/a5/cmake-build-debug/CMakeFiles/tinyrender.dir/src/core/integrator.cpp.o"
+  "/Users/anuragkalra/workspaceComp598/a5_starter/a5/src/core/renderer.cpp" "/Users/anuragkalra/workspaceComp598/a5_starter/a5/cmake-build-debug/CMakeFiles/tinyrender.dir/src/core/renderer.cpp.o"
+  "/Users/anuragkalra/workspaceComp598/a5_starter/a5/src/core/renderpass.cpp" "/Users/anuragkalra/workspaceComp598/a5_starter/a5/cmake-build-debug/CMakeFiles/tinyrender.dir/src/core/renderpass.cpp.o"
+  "/Users/anuragkalra/workspaceComp598/a5_starter/a5/src/main.cpp" "/Users/anuragkalra/workspaceComp598/a5_starter/a5/cmake-build-debug/CMakeFiles/tinyrender.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.8/include/SDL2"
+  "/usr/local/include"
+  "../src"
+  "../externals"
+  "../externals/glm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tinyrender.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyrender.dir/src/core/integrator.cpp.o"
+  "CMakeFiles/tinyrender.dir/src/core/renderer.cpp.o"
+  "CMakeFiles/tinyrender.dir/src/core/renderpass.cpp.o"
+  "CMakeFiles/tinyrender.dir/src/main.cpp.o"
+  "tinyrender.pdb"
+  "tinyrender"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyrender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
