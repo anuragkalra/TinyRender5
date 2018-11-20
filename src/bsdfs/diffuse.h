77645465bed0a466c2587846718a29b282d66258
anuragkalra <anuragkalra@outlook.com>
2018-11-20 03:00:22 -0500

    refactored diffuse and phong brdf

--- src/bsdfs/diffuse.h
+++ src/bsdfs/diffuse.h
@@ -14,65 +14,66 @@ TR_NAMESPACE_BEGIN
 /**
  * Perfectly diffuse, Lambertian reflectance model
  */
-struct DiffuseBSDF : BSDF {
-    std::unique_ptr<Texture < v3f>> albedo;
+    struct DiffuseBSDF : BSDF {
+        std::unique_ptr<Texture < v3f>> albedo;
 
-    DiffuseBSDF(const WorldData& scene, const Config& config, const size_t& matID) : BSDF(scene, config, matID) {
-        const tinyobj::material_t& mat = scene.materials[matID];
+        DiffuseBSDF(const WorldData& scene, const Config& config, const size_t& matID) : BSDF(scene, config, matID) {
+            const tinyobj::material_t& mat = scene.materials[matID];
 
-        if (mat.diffuse_texname.empty())
-            albedo = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.diffuse)));
-        else
-            albedo = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.diffuse_texname));
+            if (mat.diffuse_texname.empty())
+                albedo = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.diffuse)));
+            else
+                albedo = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.diffuse_texname));
 
-        components.push_back(EDiffuseReflection);
+            components.push_back(EDiffuseReflection);
 
-        combinedType = 0;
-        for (size_t i = 0; i < components.size(); ++i)
-            combinedType |= components[i];
-    }
+            combinedType = 0;
+            for (size_t i = 0; i < components.size(); ++i)
+                combinedType |= components[i];
+        }
 
-    // TODO - COMPLETE
-    v3f eval(const SurfaceInteraction& i) const override {
-        v3f val(0.f);
+        // TODO - COMPLETE
+        v3f eval(const SurfaceInteraction& i) const override {
+            v3f val(0.f);
 
-        if(Frame::cosTheta(i.wo) <= 0 || Frame::cosTheta(i.wi) <= 0) {
+            if(Frame::cosTheta(i.wo) <= 0 || Frame::cosTheta(i.wi) <= 0) {
+                return val;
+            }
+
+            return (albedo->eval(worldData, i)) * INV_PI * max(0.f, Frame::cosTheta(i.wi));
+        }
+
+        // TODO - COMPLETE
+        float pdf(const SurfaceInteraction& i) const override {
+            float pdf = 0.f;
+
+            if(Frame::cosTheta(i.wo) <= 0 || Frame::cosTheta(i.wi) <= 0) {
+                return pdf;
+            }
+            pdf = max(i.wi.z * INV_PI, 0.f);
+            return pdf;
+        }
+
+        // TODO - COMPLETE
+        v3f sample(SurfaceInteraction& i, const v2f& sample, float* pdf_param) const override {
+            v3f val(0.f);
+
+            i.wi = Warp::squareToCosineHemisphere(sample);
+
+            float pdf_val = pdf(i);
+            *pdf_param = pdf_val;
+
+            v3f brdf_scaling_factor = eval(i);
+
+            if (pdf_val > 0.f) {
+                val = (brdf_scaling_factor / pdf_val);
+            } else {
+                val = v3f(0.f);
+            }
             return val;
         }
 
-        return (albedo->eval(worldData, i)) * INV_PI * max(0.f, Frame::cosTheta(i.wi));
-    }
-
-    // TODO - COMPLETE
-    float pdf(const SurfaceInteraction& i) const override {
-        float pdf = 0.f;
-
-        if(Frame::cosTheta(i.wo) <= 0 || Frame::cosTheta(i.wi) <= 0) {
-            return pdf;
-        }
-        pdf = max(i.wi.z/ M_PI , 0.f);
-        return pdf;
-    }
-
-    // TODO - COMPLETE
-    v3f sample(SurfaceInteraction& i, const v2f& sample, float* pdf_param) const override {
-        v3f val(0.f);
-
-        i.wi = Warp::squareToCosineHemisphere(sample);
-
-        float pdf_val = pdf(i);
-        *pdf_param = pdf_val;
-
-        v3f brdf_factor = eval(i);
-
-        if (pdf_val > 0.f)
-            val = brdf_factor / pdf_val;
-        else
-            val = v3f(0);
-        return val;
-    }
-
-    std::string toString() const override { return "Diffuse"; }
-};
+        std::string toString() const override { return "Diffuse"; }
+    };
 
 TR_NAMESPACE_END
--- src/bsdfs/phong.h
+++ src/bsdfs/phong.h
@@ -14,103 +14,106 @@ TR_NAMESPACE_BEGIN
 /**
  * Modified Phong reflectance model
  */
-struct PhongBSDF : BSDF {
+    struct PhongBSDF : BSDF {
 
-    std::unique_ptr<Texture < v3f>> specularReflectance;
-    std::unique_ptr<Texture < v3f>> diffuseReflectance;
-    std::unique_ptr<Texture < float>> exponent;
-    float specularSamplingWeight;
-    float scale;
+        std::unique_ptr<Texture < v3f>> specularReflectance;
+        std::unique_ptr<Texture < v3f>> diffuseReflectance;
+        std::unique_ptr<Texture < float>> exponent;
+        float specularSamplingWeight;
+        float scale;
 
-    PhongBSDF(const WorldData& scene, const Config& config, const size_t& matID) : BSDF(scene, config, matID) {
-        const tinyobj::material_t& mat = scene.materials[matID];
+        PhongBSDF(const WorldData& scene, const Config& config, const size_t& matID) : BSDF(scene, config, matID) {
+            const tinyobj::material_t& mat = scene.materials[matID];
 
-        if (mat.specular_texname.empty())
-            specularReflectance = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.specular)));
-        else
-            specularReflectance = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.specular_texname));
+            if (mat.specular_texname.empty())
+                specularReflectance = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.specular)));
+            else
+                specularReflectance = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.specular_texname));
 
-        if (mat.diffuse_texname.empty())
-            diffuseReflectance = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.diffuse)));
-        else
-            diffuseReflectance = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.diffuse_texname));
+            if (mat.diffuse_texname.empty())
+                diffuseReflectance = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.diffuse)));
+            else
+                diffuseReflectance = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.diffuse_texname));
 
-        exponent = std::unique_ptr<Texture<float>>(new ConstantTexture1f(mat.shininess));
+            exponent = std::unique_ptr<Texture<float>>(new ConstantTexture1f(mat.shininess));
 
-        //get scale value to ensure energy conservation
-        v3f maxValue = specularReflectance->getMax() + diffuseReflectance->getMax();
-        float actualMax = max(max(maxValue.x, maxValue.y), maxValue.z);
-        scale = actualMax > 1.0f ? 0.99f * (1.0f / actualMax) : 1.0f;
+            //get scale value to ensure energy conservation
+            v3f maxValue = specularReflectance->getMax() + diffuseReflectance->getMax();
+            float actualMax = max(max(maxValue.x, maxValue.y), maxValue.z);
+            scale = actualMax > 1.0f ? 0.99f * (1.0f / actualMax) : 1.0f;
 
-        float dAvg = getLuminance(diffuseReflectance->getAverage() * scale);
-        float sAvg = getLuminance(specularReflectance->getAverage() * scale);
-        specularSamplingWeight = sAvg / (dAvg + sAvg);
+            float dAvg = getLuminance(diffuseReflectance->getAverage() * scale);
+            float sAvg = getLuminance(specularReflectance->getAverage() * scale);
+            specularSamplingWeight = sAvg / (dAvg + sAvg);
 
-        components.push_back(EGlossyReflection);
-        components.push_back(EDiffuseReflection);
+            components.push_back(EGlossyReflection);
+            components.push_back(EDiffuseReflection);
 
-        combinedType = 0;
-        for (unsigned int component : components)
-            combinedType |= component;
-    }
-
-    inline v3f reflect(const v3f& d) const {
-        return v3f(-d.x, -d.y, d.z);
-    }
-
-    // TODO - COMPLETE
-    v3f eval(const SurfaceInteraction& i) const override {
-        v3f val(0.f);
-        v3f wo = i.wo;
-        v3f wi = i.wi;
-        float z_in = Frame::cosTheta(wi);
-        float z_out = Frame::cosTheta(wo);
-        if (z_in > 0 && z_out > 0) { //check correct direction
-            v3f rho_d = diffuseReflectance->eval(worldData, i);
-            v3f rho_s = specularReflectance->eval(worldData, i);
-            float n = exponent->eval(worldData, i);
-            float cos_a = fmax(glm::dot(glm::normalize(reflect(wo)), wi), 0.0);
-            val = rho_d * INV_PI + rho_s * INV_TWOPI * (n + 2)*pow(cos_a, n);
-            return val * z_in * scale;
+            combinedType = 0;
+            for (unsigned int component : components)
+                combinedType |= component;
         }
-        else {
-            return v3f(0);
+
+        inline v3f reflect(const v3f& d) const {
+            return v3f(-d.x, -d.y, d.z);
         }
-    }
 
-    // TODO - COMPLETE
-    float pdf(const SurfaceInteraction& i) const override {
-        v3f refl = reflect(i.wo); //get reflection dir in local coords
-        v3f v = glm::toMat4(glm::quat(refl, v3f(0, 0, 1))) * v4f(i.wi, 1); //rotate the lobe to be around the z axis in local
+        // TODO - COMPLETE
+        v3f eval(const SurfaceInteraction& i) const override {
+            v3f val(0.f);
+            v3f wo = i.wo;
+            v3f wi = i.wi;
 
-        //Warp::squareToPhongLobePdf needs phong lobe centered around z-axis
-        float pdf = Warp::squareToPhongLobePdf(v, exponent->eval(worldData, i));
-        return pdf;
-    }
+            float z_in = Frame::cosTheta(i.wi);
+            float z_out = Frame::cosTheta(i.wo);
 
-    // TODO - COMPLETE
-    v3f sample(SurfaceInteraction& i, const v2f& _sample, float* pdf_param) const override {
-        v3f val(0.f);
-        v3f wi = Warp::squareToPhongLobe(_sample, exponent->eval(worldData, i));
-
-        v3f refl = reflect(i.wo); //get reflection dir in local coords
-        wi = glm::toMat4(glm::quat(v3f(0, 0, 1), refl)) * v4f(wi, 1); //rotate the lobe to be around the reflection dir in local
-        i.wi = wi; //set the i.wi to be rotated around the relection of wo
-
-        float pdf_val = pdf(i);
-        *pdf_param = pdf_val;
-
-        if (pdf_val == 0.f) { // check if pdf val is well defined
-            return v3f(0);
+            if (z_in > 0 && z_out > 0) {
+                v3f rho_d = diffuseReflectance->eval(worldData, i);
+                v3f rho_s = specularReflectance->eval(worldData, i);
+                float n = exponent->eval(worldData, i);
+                float cos_a = fmax(glm::dot(glm::normalize(reflect(i.wo)), i.wi), 0.0);
+                val = rho_d * INV_PI + rho_s * INV_TWOPI * (n + 2)*pow(cos_a, n);
+                return val * z_in * scale;
+            }
+            else {
+                return v3f(0.f);
+            }
         }
-        else {
-            v3f brdf_factor = eval(i);
-            val = brdf_factor / pdf_val;
-            return val;
-        }
-    }
 
-    std::string toString() const override { return "Phong"; }
-};
+        // TODO - COMPLETE
+        float pdf(const SurfaceInteraction& i) const override {
+
+            v3f wOut = i.wo;
+            v3f wIn = i.wi;
+            v3f refl = reflect(wOut);
+            v3f v = glm::toMat4(glm::quat(refl, v3f(0, 0, 1))) * v4f(wIn, 1);
+
+            return Warp::squareToPhongLobePdf(v, exponent->eval(worldData, i));
+        }
+
+        // TODO - COMPLETE
+        v3f sample(SurfaceInteraction& i, const v2f& _sample, float* pdf_param) const override {
+            v3f val(0.f);
+            float EXP = exponent->eval(worldData, i);
+            v3f wi = Warp::squareToPhongLobe(_sample, EXP);
+
+            v3f refl = reflect(i.wo);
+            wi = glm::toMat4(glm::quat(v3f(0, 0, 1), refl)) * v4f(wi, 1);
+            i.wi = wi;
+
+            float pdf_val = pdf(i);
+            *pdf_param = pdf_val;
+
+            if (pdf_val == 0.f) {
+                return v3f(0.f);
+            } else {
+                v3f brdf_scaling_factor = eval(i);
+                val = brdf_scaling_factor / pdf_val;
+                return val;
+            }
+        }
+
+        std::string toString() const override { return "Phong"; }
+    };
 
 TR_NAMESPACE_END
